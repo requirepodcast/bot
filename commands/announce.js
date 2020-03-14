26215cbc302adaa4bf10ef819fc29949e4e46e35
datejer <ejer.priv@wp.pl>
2020-03-14 12:36:28 +0100

    add help command + polishify

--- commands/announce.js
+++ commands/announce.js
@@ -3,32 +3,34 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
 	let staff = message.guild.roles.find(r => r.name === "require('prowadzący')");
 	if (!message.member.roles.has(staff.id))
-		return message.reply("Insufficient permissions.");
+		return message.reply("Brak uprawnień.");
 
 	let announcements = message.guild.channels.find(
 		c => c.name === "ogłoszenia" && c.parent.name === "require('info');"
 	);
 
 	let argument = args.join(" ");
-	let [title, content] = argument.split(" | ");
+	let [title, content, ping] = argument.split(" | ");
 
-	if (!title) return message.reply("Please enter the announcement title.");
-	if (!content) return message.reply("Please enter the announcement content.");
+	if (!title) return message.reply("Proszę wpisać tytuł ogłoszenia.");
+	if (!content) return message.reply("Proszę wpisać treść ogłoszenia.");
 
 	let embed = new Discord.RichEmbed()
 		.setTitle(title.toString())
 		.setDescription(content.toString())
 		.setColor(client.config.colors.primary)
-		.setFooter(`~ ${message.member.nickname}`)
+		.setFooter(`~ ${message.member.nickname || message.author.username}`)
 		.setTimestamp();
 
-	announcements.send("@everyone").then(m => m.delete(1000));
+	if (ping && ping === "true")
+		announcements.send("@everyone").then(m => m.delete(1000));
 	announcements.send(embed);
+	message.reply(`Wysłano ogłoszenie ${announcements.toString()}`);
 };
 
 exports.help = {
 	name: "announce",
-	description: "Send a public announcement in the #ogłoszenia channel.",
-	usage: "announce <title> | <content>",
+	description: "Wysyła ogłoszenie w kanale #ogłoszenia.",
+	usage: "announce <title> | <content> | [ping ? true]",
 	staff: true
 };
---
+++ commands/help.js
@@ -0,0 +1,70 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+	let staff = message.guild.roles.find(r => r.name === "require('prowadzący')");
+
+	if (!args[0]) {
+		let embed = new Discord.RichEmbed()
+			.setAuthor(message.author.tag, message.author.avatarURL)
+			.setColor(client.config.colors.primary)
+			.setDescription(`Wysłałem do ciebię listę komend! Sprawdź DM.`);
+
+		let help = new Discord.RichEmbed()
+			.setTitle("Komendy")
+			.setColor(client.config.colors.primary);
+
+		if (!message.member.roles.has(staff.id))
+			help.setDescription(
+				"Użyj `-help [command]` aby uzyskać informacje o danej komendzie.\n\n" +
+					client.commands
+						.filter(cmd => !cmd.help.staff)
+						.map(cmd => `\`${cmd.help.name}\` - ${cmd.help.description}`)
+						.join("\n")
+			);
+		else
+			help.setDescription(
+				"Użyj `-help [command]` aby uzyskać informacje o danej komendzie.\n\n" +
+					client.commands
+						.map(cmd => `\`${cmd.help.name}\` - ${cmd.help.description}`)
+						.join("\n")
+			);
+
+		message.author.send(help).catch(() => {
+			let error = new Discord.RichEmbed()
+				.setAuthor("Wystąpił błąd!", "https://i.imgur.com/FCZNSQa.png")
+				.setDescription("Nie mogłem wysłać do ciebie wiadomości!")
+				.setColor(client.config.colors.secondary)
+				.setTimestamp();
+
+			message.channel.send(error);
+			return message.delete();
+		});
+
+		message.channel.send(embed);
+		message.delete();
+	} else if (args[0]) {
+		let command = client.commands.get(args[0]);
+		if (!command) return message.reply("Proszę wpisać poprawną komendę!");
+
+		let props = require(`./${args[0]}.js`);
+
+		if (!message.member.roles.has(staff.id) && props.help.staff)
+			return message.reply("Proszę wpisać poprawną komendę!");
+
+		let embed = new Discord.RichEmbed()
+			.setTitle(`Komenda`)
+			.setColor(client.config.colors.primary)
+			.setDescription(
+				`**Nazwa:** ${props.help.name}\n**Opis:** ${props.help.description}\n**Sposób użycia:** ${client.config.prefix}${props.help.usage}`
+			);
+
+		message.channel.send(embed);
+		message.delete();
+	}
+};
+
+exports.help = {
+	name: "help",
+	description: "Wysyła listę komend lub dane odnośnie wybranej komendy.",
+	usage: "help [command]"
+};
