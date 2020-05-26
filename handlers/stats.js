89ef02ff3ad864af59de075518769b18199a4980
datejer <ejer.priv@wp.pl>
2020-05-26 12:46:43 +0200

    Split stats handling

---
+++ handlers/stats.js
@@ -0,0 +1,26 @@
+const Parser = require("rss-parser");
+const parser = new Parser();
+
+const update = client => {
+	let guild = client.guilds.find(g => g.id === client.config.guild);
+	let channel1 = guild.channels.find(c => c.id === "712226628813717515");
+	let channel2 = guild.channels.find(c => c.id === "712230478949646467");
+	let channel3 = guild.channels.find(c => c.id === "712233442984591431");
+
+	parser
+		.parseURL("https://anchor.fm/s/139df89c/podcast/rss")
+		.then(({ items }) => {
+			channel1.setName(`Odcinki: ${items.length}`);
+			let duration = 0;
+			for (let episode of items) {
+				duration += parseInt(episode.itunes.duration);
+			}
+			let time = new Date(duration * 1000).toISOString().substr(11, 8);
+			channel2.setName(`Łączna długość: ${time}`);
+			channel3.setName(
+				`Srednia długość: ${Math.ceil(duration / items.length / 60) + " min."}`
+			);
+		});
+};
+
+module.exports = { update };
