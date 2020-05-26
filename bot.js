8dcdcf98f91a4678929c1316c3ddb389bb54e2c6
datejer <ejer.priv@wp.pl>
2020-05-26 12:40:18 +0200

    Add render server notification webhook

--- bot.js
+++ bot.js
@@ -2,12 +2,14 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
 
 require("dotenv").config();
 
 const client = new Discord.Client();
 const config = require("./config.js");
 client.config = config;
+client.playing = false;
 const app = express();
 const port = 3000;
 
@@ -16,6 +18,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => res.status(200).send("Hello World!"));
+
+setInterval(() => {
+	http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 260000);
 
 app.post("/announcement/webhook", (req, res) => {
 	if (req.headers.authorization !== process.env.WEBHOOK_AUTHORIZATION)
@@ -27,11 +33,11 @@ app.post("/announcement/webhook", (req, res) => {
 	if (announcements.sent.includes(req.body.sys.id))
 		return res.status(302).json({ message: "Announcement already posted" });
 
-	let guild = client.guilds.find((g) => g.id === client.config.guild);
+	let guild = client.guilds.find(g => g.id === client.config.guild);
 	let channel = guild.channels.find(
-		(c) => c.id === client.config.announcementChannel
+		c => c.id === client.config.announcementChannel
 	);
-	let role = guild.roles.find((r) => r.id === client.config.episodePing);
+	let role = guild.roles.find(r => r.id === client.config.episodePing);
 
 	let emoji =
 		client.config.emojis[
@@ -61,11 +67,48 @@ app.post("/announcement/webhook", (req, res) => {
 		.json({ message: "Succesfully sent Discord announcement." });
 });
 
+app.post("/render/webhook", (req, res) => {
+	if (req.headers.authorization !== process.env.WEBHOOK_AUTHORIZATION)
+		return res.status(401).json({ message: "Unauthorized" });
+
+	let guild = client.guilds.find(g => g.id === client.config.guild);
+	let channel = guild.channels.find(c => c.id === "712586635417747479");
+	let role = guild.roles.find(r => r.id === "675309294748565515");
+
+	let embed = new Discord.RichEmbed()
+		.setAuthor(
+			"Require Podcast Render Server",
+			"https://i.imgur.com/ZHV3sG1.png",
+			"http://requirepodcast-render-server.herokuapp.com/"
+		)
+		.setTimestamp()
+		.setTitle(req.body.title)
+		.setDescription(req.body.description)
+		.setColor(client.config.colors.primary);
+
+	if (req.body.user && !req.body.userAvatar)
+		embed.setFooter(`Action performed by ${req.body.user}`);
+	if (req.body.user && req.body.userAvatar)
+		embed.setFooter(
+			`Action performed by ${req.body.user}`,
+			req.body.userAvatar
+		);
+
+	channel.send(`${role}`).then(channel.send(embed));
+	if (req.body.attachment)
+		channel.send(new Discord.Attachment(req.body.attachment));
+	if (req.body.attachment2)
+		channel.send(new Discord.Attachment(req.body.attachment2));
+	return res
+		.status(201)
+		.json({ message: "Succesfully sent Discord announcement." });
+});
+
 app.listen(port, () => console.log(`Server listening on port ${port}`));
 
 fs.readdir("./events/", (err, files) => {
 	if (err) return console.error(err);
-	files.map((file) => {
+	files.map(file => {
 		let event = require(`./events/${file}`);
 		let eventName = file.split(".")[0];
 		client.on(eventName, event.bind(null, client));
@@ -77,7 +120,7 @@ client.commands = new Discord.Collection();
 fs.readdir("./commands/", (err, files) => {
 	if (err) return console.error(err);
 	console.log("[Commands] Loading...");
-	files.map((file) => {
+	files.map(file => {
 		if (!file.endsWith(".js")) return;
 		let props = require(`./commands/${file}`);
 		console.log(`[Commands] Loaded ${file}`);
